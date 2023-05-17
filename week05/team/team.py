da29b7d393d1819970050783a4db32ab0d9ab4c9
Stephen Nielsen <smnielsen256@byui.edu>
2023-05-17 11:34:44 -0600

    Progress on team assignment

--- week05/team/team.py
+++ week05/team/team.py
@@ -41,9 +41,47 @@ def is_prime(n: int) -> bool:
         i += 6
     return True
 
+class Queue251():
+    """ This is the queue object to use for this assignment. Do not modify!! """
+
+    def __init__(self):
+        self.items = []
+
+    def size(self):
+        return len(self.items)
+
+    def put(self, item):
+        assert len(self.items) <= 10
+        self.items.append(item)
+
+    def get(self):
+        return self.items.pop(0)
+    
 # TODO create read_thread function
+def read_thread(url, q, sem_queue_empty):
+    with open(url) as file:
+        for line in file:
+            q.put(line)
+            sem_queue_empty.release()
+        q.put("FINAL ENTRY")
+
 
 # TODO create prime_process function
+def prime_process(q, list_of_primes, sem_queue_empty):
+    while True:
+        sem_queue_empty.acquire()
+        entry = q.get()
+
+        #check if final entry
+        if entry == "FINAL ENTRY":
+            q.put(entry)
+            break
+        else:
+            if is_prime(entry):
+                list_of_primes.append(entry)
+
+
+    
 
 def create_data_txt(filename):
     # only create if is doesn't exist 
@@ -63,14 +101,28 @@ def main():
     log.start_timer()
 
     # TODO Create shared data structures
+    q = Queue251()
+    sem_queue_empty = threading.Semaphore(0)
+    url = 'data.txt'
+    process_list = []
+    primes = []
 
     # TODO create reading thread
+    reader = threading.Thread(target=read_thread, args=(url, q, sem_queue_empty))
 
     # TODO create prime processes
+    for _ in range(0, PRIME_PROCESS_COUNT):
+        process_list.append(mp.Process(target=prime_process, args=(q, primes, sem_queue_empty)))
 
     # TODO Start them all
+    reader.start()
+    for i in range(0, PRIME_PROCESS_COUNT):
+        process_list[i].start()
 
     # TODO wait for them to complete
+    reader.join()
+    for i in range(0, PRIME_PROCESS_COUNT):
+        process_list[i].join()
 
     log.stop_timer(f'All primes have been found using {PRIME_PROCESS_COUNT} processes')
 
