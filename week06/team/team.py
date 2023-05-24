d213b6a2ae27076af6e7ce3fb8ba60580d1dc69b
Stephen Nielsen <smnielsen256@byui.edu>
2023-05-24 11:07:22 -0600

    Progress on team project

--- week06/team/team.py
+++ week06/team/team.py
@@ -22,7 +22,7 @@ import filecmp
 # Include cse 251 common Python files
 from cse251 import *
 
-def sender():
+def sender(pipe_in, url, word_counter):
     """ function to send messages to other end of pipe """
     '''
     open the file
@@ -30,17 +30,32 @@ def sender():
     Note: you must break each line in the file into words and
           send those words through the pipe
     '''
-    pass
+    with open(url) as file:
+        for line in file:
+            for word in line:
+                # send words and a space
+                pipe_in.send(word)
+                word_counter.value += 1
+                # send a return after each line
+                #pipe_in.send("\n")
+                print("sent")
 
 
-def receiver():
+def receiver(pipe_out, url, word_counter):
     """ function to print the messages received from other end of pipe """
     ''' 
     open the file for writing
     receive all content through the shared pipe and write to the file
     Keep track of the number of items sent over the pipe
     '''
-    pass
+    words_recieved = 0
+    with open(url, 'w') as file:
+        while words_recieved < word_counter.value:
+            word = pipe_out.recv()
+            file.write(word)
+            print(word)
+            words_recieved += 1
+            
 
 
 def are_files_same(filename1, filename2):
@@ -50,22 +65,33 @@ def are_files_same(filename1, filename2):
 
 def copy_file(log, filename1, filename2):
     # TODO create a pipe 
+    pipe_in, pipe_out = mp.Pipe()
     
     # TODO create variable to count items sent over the pipe
+    word_counter = mp.Value('i', 0)
 
     # TODO create processes 
+    process_sender = mp.Process(target=sender, args=(pipe_in, filename1, word_counter))
+    process_reciever = mp.Process(target=receiver, args=(pipe_out, filename2, word_counter))
+
 
     log.start_timer()
     start_time = log.get_time()
 
     # TODO start processes 
-    
+    process_sender.start()
+    process_reciever.start()
+    print("started")
+
     # TODO wait for processes to finish
+    process_sender.join()
+    process_reciever.join()
+    print("joined")
 
     stop_time = log.get_time()
 
-    log.stop_timer(f'Total time to transfer content = {PUT YOUR VARIABLE HERE}: ')
-    log.write(f'items / second = {PUT YOUR VARIABLE HERE / (stop_time - start_time)}')
+    log.stop_timer(f'Total time to transfer content = {(stop_time - start_time)}: ')
+    log.write(f'items / second = {word_counter.value / (stop_time - start_time)}')
 
     if are_files_same(filename1, filename2):
         log.write(f'{filename1} - Files are the same')
