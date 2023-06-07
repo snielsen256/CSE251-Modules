ac23f6846d0a18109d70ab393c7bb15b747c63d6
Stephen Nielsen <smnielsen256@byui.edu>
2023-06-07 11:09:02 -0600

    progress on team activity

--- week08/team/team.py
+++ week08/team/team.py
@@ -74,7 +74,54 @@ def merge_normal(arr):
 def merge_sort_thread(arr):
     # TODO - Add your code here to use threads.  Each time the merge algorithm does a recursive
     #        call, you need to create a thread to handle that call
-    pass
+
+    
+    # base case of the recursion - must have at least 2+ items
+    if len(arr) > 1:
+         # Finding the mid of the array
+        mid = len(arr) // 2
+ 
+        # Dividing the array elements
+        L = arr[:mid]
+ 
+        # into 2 halves
+        R = arr[mid:]
+ 
+        # Sorting the first half
+        l_thread = threading.Thread(target=merge_sort_thread, args=(L,))
+        #print(f"L: {L}")
+        l_thread.start()
+        l_thread.join()
+ 
+        # Sorting the second half
+        r_thread = threading.Thread(target=merge_sort_thread, args=(R,))
+        #print(f"R: {R}")
+        r_thread.start()
+        r_thread.join()
+
+ 
+        i = j = k = 0
+ 
+        # Copy data to temp arrays L[] and R[]
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+ 
+        # Checking if any element was left
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+ 
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
 
 
 # -----------------------------------------------------------------------------
