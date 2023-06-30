5faa8e6d3559224a984d6664a793507efdef1d03
Stephen Nielsen <smnielsen256@byui.edu>
2023-06-30 11:12:40 -0600

    Prograss on team activity 2

--- week11/team/team2.py
+++ week11/team/team2.py
@@ -45,23 +45,113 @@ import multiprocessing as mp
 
 # -------------------------------------------------------------------
 class Queue_t:
-	pass
+    def __init__(self):
+        self.arr = []
+        self.lock = threading.Lock()
+
+    def size(self):
+        self.lock.acquire()
+        size = len(self.arr)
+        self.lock.release()
+        return size
+
+    def put(self, item):
+        self.lock.acquire()
+        self.arr.append(item)
+        self.lock.release()
+
+    def get(self):
+        self.lock.acquire()
+        item = self.arr.pop(0)
+        self.lock.release()
+        return item
 
 # -------------------------------------------------------------------
 class Stack_t:
-	pass
+    def __init__(self):
+        self.arr = []
+        self.lock = threading.Lock()
+
+    def push(self, item):
+        self.lock.acquire()
+        self.arr.append(item)
+        self.lock.release()
+
+    def pop(self):
+        self.lock.acquire()
+        item = self.arr.pop(-1)
+        self.lock.release()
+        return item
 
 # -------------------------------------------------------------------
 class Queue_p:
-	pass
+    def __init__(self):
+        self.arr = []
+        self.lock = mp.Lock()
+
+    def size(self):
+        self.lock.acquire()
+        size = len(self.arr)
+        self.lock.release()
+        return size
+
+    def put(self, item):
+        self.lock.acquire()
+        self.arr.append(item)
+        self.lock.release()
+
+    def get(self):
+        self.lock.acquire()
+        item = self.arr.pop(0)
+        self.lock.release()
+        return item
 
 # -------------------------------------------------------------------
 class Stack_p:
-	pass
+    def __init__(self):
+        self.arr = []
+        self.lock = mp.Lock()
+
+    def push(self, item):
+        self.lock.acquire()
+        self.arr.append(item)
+        self.lock.release()
+
+    def pop(self):
+        self.lock.acquire()
+        item = self.arr.pop(-1)
+        self.lock.release()
+        return item
+
+def test_q_t(q):
+    print("Put 5")
+    q.put(5)
+    print(f"Size: {q.size()}")
+    print(f"Get: {q.get()}")
 
 
 def main():
     pass
+    # test Queue_t
+    test_list = []
+    q = Queue_t()
+    for i in range(3):
+        test_list.append(threading.Thread(target=test_q_t, args=(q,)))
+    
+    for i in test_list:
+        i.start()
+
+    for i in test_list:
+        i.join()
+    
+    
+    # test Stack_t
+
+    # test Queue_p
+
+    # test Stack_p
+
+
 
 if __name__ == '__main__':
     main()
